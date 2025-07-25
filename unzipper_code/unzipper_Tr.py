56f829f0c2471edf1cc6ea8cac430696fa42ee62
Rishiraj Bal <rishirajbal003@gmail.com>
2025-07-26 01:47:07 +0530

    Update unzipper_Tr.py

--- unzipper_code/unzipper_Tr.py
+++ unzipper_code/unzipper_Tr.py
@@ -2,16 +2,16 @@ import gzip
 import shutil
 import os
 
-# Updated paths
+
 source_folder = r'C:\Users\KIIT\OneDrive\Desktop\Resume bulider\projects\Tumour_detection_project\datasets\Task01_BrainTumour\imagesTr'
 destination_folder = r'C:\Users\KIIT\OneDrive\Desktop\Resume bulider\projects\Tumour_detection_project\datasets\Task01_BrainTumour\imagesTr _unzipped'
 
-# Make sure destination exists
+
 os.makedirs(destination_folder, exist_ok=True)
 
-# Go through each file
+
 file_list = os.listdir(source_folder)
-print(f"📂 Found {len(file_list)} files in the source folder.")
+print(f" Found {len(file_list)} files in the source folder.")
 
 for filename in file_list:
     if filename.startswith('._'):
@@ -23,10 +23,10 @@ for filename in file_list:
         destination_filename = filename.replace('.nii.gz', '.nii')
         destination_path = os.path.join(destination_folder, destination_filename)
 
-        print(f"🔄 Unzipping {filename} -> {destination_filename}")
+        print(f" Unzipping {filename} -> {destination_filename}")
         
         with gzip.open(source_path, 'rb') as f_in:
             with open(destination_path, 'wb') as f_out:
                 shutil.copyfileobj(f_in, f_out)
 
-print("\n🎯 Done unzipping all .nii.gz files into the destination folder!")
+print("\n Done unzipping all .nii.gz files into the destination folder!")
